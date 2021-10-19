6b8bebdc723b3ba7072d711cdd70b0bc5906bd1d
ben-edde <ben.0xedde@gmail.com>
2021-10-19 16:37:13 +0800

    update utils;

--- utils/filter_news_wrt_price.py
+++ utils/filter_news_wrt_price.py
@@ -3,9 +3,9 @@ Both news and prices data would be used. It is always more news date than price 
 """
 import pandas as pd
 df_news = pd.read_csv("RedditNews.csv")
+df_news.Date=pd.to_datetime(df_news.Date)
 df_price=pd.read_csv("Cushing_OK_WTI_Spot_Price_FOB.csv")
 df_price.Date=pd.to_datetime(df_price.Date)
-df_price.set_index("Date",inplace=True)
 
 df_filtered_news=df_news[df_news.Date.isin(df_price.Date)]
 
