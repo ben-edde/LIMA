996fc8ef33ff9d122577bcba272fc21e5f1f4895
ben-edde <ben.0xedde@gmail.com>
2021-11-03 21:37:47 +0800

    updated ARIMA w/ season;

--- benchmark/ARIMA_t_t3.py
+++ benchmark/ARIMA_t_t3.py
@@ -6,7 +6,7 @@ from sacred import Experiment
 from sacred.observers import FileStorageObserver
 
 exp = Experiment('ARIMA_benchmark')
-exp.observers.append(FileStorageObserver('runs'))
+exp.observers.append(FileStorageObserver('auto'))
 
 
 def evaluate(y_true, y_pred, horizon):
@@ -17,33 +17,16 @@ def evaluate(y_true, y_pred, horizon):
     # MAPE
     mape = mean_absolute_percentage_error(y_true, y_pred)
     return pd.DataFrame({
-        "horizon": [horizon],
-        "rmse": [rmse],
-        "mae": [mae],
-        "mape": [mape]
+        f"rmse_{horizon}": [rmse],
+        f"mae_{horizon}": [mae],
+        f"mape_{horizon}": [mape]
     })
 
 
-@exp.config
-def set_paramaters():
-    p, d, q = 1, 1, 1
-
-
-@exp.automain
-def main(p, d, q):
-    df_price = pd.read_csv("WTI_Spot_2008-06-09_2016-07-01.csv")
-    df_price.Date = pd.to_datetime(df_price.Date, infer_datetime_format=True)
-
-    df_price["Price_t+3"] = df_price.shift(0)["Price"]
-    df_price["Price_t+2"] = df_price.shift(-1)["Price"]
-    df_price["Price_t+1"] = df_price.shift(-2)["Price"]
-    df_price["Price_t"] = df_price.shift(-3)["Price"]
-    df_price.dropna(inplace=True)
-    # due to price data order from latest, need to invert order before forecast
-    df_price = df_price.iloc[::-1]
-    df_price.reset_index(inplace=True, drop=True)
-
-    arima_model = ARIMA(df_price['Price_t'], order=(p, d, q))
+def run_arima(df_price, p, d, q, P, D, Q, s):
+    arima_model = ARIMA(df_price['Price_t'],
+                        order=(p, d, q),
+                        seasonal_order=(P, D, Q, s))
     arima_result = arima_model.fit()
     print(arima_result.summary())
 
@@ -73,8 +56,47 @@ def main(p, d, q):
                          df_price["predicted_t+3"],
                          horizon=3)
 
-    evaluation_result = metric_t.append([metric_t1, metric_t2, metric_t3],
-                                        ignore_index=True)
+    df_order = pd.DataFrame({
+        "p": [p],
+        "d": [d],
+        "q": [q],
+        "P": [P],
+        "D": [D],
+        "Q": [Q],
+        "s": [s],
+    })
+    evaluation_result = pd.concat(
+        [df_order, metric_t, metric_t1, metric_t2, metric_t3], axis=1)
 
-    result_dict = evaluation_result.to_dict()
-    [exp.log_scalar(each, result_dict[each]) for each in result_dict]
+    return evaluation_result
+
+
+@exp.automain
+def main():
+    df_price = pd.read_csv("WTI_Spot_2008-06-09_2016-07-01.csv")
+    df_price.Date = pd.to_datetime(df_price.Date, infer_datetime_format=True)
+
+    df_price["Price_t+3"] = df_price.shift(0)["Price"]
+    df_price["Price_t+2"] = df_price.shift(-1)["Price"]
+    df_price["Price_t+1"] = df_price.shift(-2)["Price"]
+    df_price["Price_t"] = df_price.shift(-3)["Price"]
+    df_price.dropna(inplace=True)
+    # due to price data order from latest, need to invert order before forecast
+    df_price = df_price.iloc[::-1]
+    df_price.reset_index(inplace=True, drop=True)
+    df_result = pd.DataFrame(columns=[
+        "p", "d", "q", "P", "D", "Q", "s", "rmse_0", "mae_0", "mape_0",
+        "rmse_1", "mae_1", "mape_1", "rmse_2", "mae_2", "mape_2", "rmse_3",
+        "mae_3", "mape_3"
+    ])
+    for p in range(6):
+        for d in range(4):
+            for q in range(6):
+                for P in range(6):
+                    for D in range(4):
+                        for Q in range(6):
+                            for s in [3, 6, 9, 12]:
+                                result = run_arima(df_price, p, d, q, P, D, Q,
+                                                   s)
+                                df_result = df_result.append(result)
+    df_result.to_csv("ARIMA_result.csv", index=False)
